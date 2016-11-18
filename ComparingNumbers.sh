f997234e891aef0e8db798c129087943dcd8f39c
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-11-17 22:03:42 -0800

    init

---
+++ ComparingNumbers.sh
@@ -0,0 +1,9 @@
+read X
+read Y
+if [ $X -gt $Y ]; then
+    echo "X is greater than Y"
+elif [ $X -lt $Y ]; then
+    echo "X is less than Y"
+else
+    echo "X is equal to Y"
+fi
---
+++ Conditionals.sh
@@ -0,0 +1,6 @@
+read ch
+if [ $ch == "Y" ] || [ $ch == "y" ]; then
+    echo "YES"
+elif [ $ch == "N" ] || [ $ch == "n" ]; then
+    echo "NO"
+fi
