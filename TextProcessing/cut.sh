385cdd14b85c661977eda06e14a09698d061a684
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-11-19 13:43:51 -0800

    cut

---
+++ TextProcessing/cut.sh
@@ -0,0 +1,15 @@
+# index start from 1, range inclusive
+
+# file column
+cut -c3
+cut -c2,7
+cut -c2-7  # range inclusive
+cut -c-4
+cut -c13-
+
+# csv, delimiter
+cut -d$'\t' -f-3
+cut -d$'\t' -f2-
+
+cut -d" " -f4
+cut -d" " -f-3
